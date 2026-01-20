d3ba16b4313fe10cdaff3f0a83ce0d24eddd2a11
pareesa969 <pareesakhan613@gmail.com>
2026-01-20 23:36:59 +0500

    feat(data): add learning profile questionnaire definition

--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import Screener from "./pages/Screener"
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-[#0A0A1A] text-white">
-      <Screener />
-    </div>
-  )
-}
